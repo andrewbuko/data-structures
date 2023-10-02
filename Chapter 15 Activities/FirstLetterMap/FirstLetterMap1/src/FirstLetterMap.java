fa53e9e135d6c33b7b90225dd2b3ff80d95f0e24
andrewbuko <89219042+andrewbuko@users.noreply.github.com>
2023-10-02 11:43:05 -0500

    Update FirstLetterMap.java

--- Chapter 15 Activities/FirstLetterMap/FirstLetterMap1/src/FirstLetterMap.java
+++ Chapter 15 Activities/FirstLetterMap/FirstLetterMap1/src/FirstLetterMap.java
@@ -12,28 +12,29 @@ public class FirstLetterMap
 {
     public static void main(String[] args)
     {
-        String filename = "src/test1.txt";
+        String filename = "Chapter 15 Activities/FirstLetterMap/FirstLetterMap1/src/test1.txt";
 
         try (Scanner in = new Scanner(new File(filename)))
         {
 
             // Create your map here
-            ...
+            Map<Character, Set<String> > map = new HashMap<>();
 
             while (in.hasNext())
             {
                 String word = clean(in.next());
                 Character c = word.charAt(0);
-
+                Set<String> qu = new HashSet<>();
                 // Update the map here
-                // Use the Java 8 merge method
-                . . .
+                qu.add(word)
+                map.merge(c,que, orig ->orig.add(word));
 
             }
 
             // Print the map here in this form
             // a: [a, able, aardvark]
-            . . .
+            for(Character a:map.keySet())
+                System.out.println(a+": "+map.get(a));
         } catch (FileNotFoundException e)
         {
             System.out.println("Cannot open: " + filename);
