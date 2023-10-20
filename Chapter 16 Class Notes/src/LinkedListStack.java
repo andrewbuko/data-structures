6e0b9d3fb0b262467ebce827824a1672fb8fe175
andrewbuko <89219042+andrewbuko@users.noreply.github.com>
2023-10-20 13:51:01 -0500

    vds

--- Chapter 16 Class Notes/src/LinkedListStack.java
+++ Chapter 16 Class Notes/src/LinkedListStack.java
@@ -20,7 +20,13 @@ public class LinkedListStack
      *
      * @param element the element to add
     */
+    public void push(Object element) {
+        Node newNode = new Node();
+        newNode.data = element;
+        newNode.next = this.first;
+        this.first = newNode;
 
+    }
 
 
 
@@ -30,6 +36,14 @@ public class LinkedListStack
         Removes the element from the top of the stack.
         @return the removed element
     */
+    public Object pop() {
+        if (this.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        Object element = this.first.data;
+        this.first = this.first.next;
+        return element;
+    }
 
 
 
@@ -43,7 +57,9 @@ public class LinkedListStack
      *
      * @return true if the stack is empty
     */
-
+    public boolean isEmpty() {
+        return this.first == null;
+    }
 
     static class Node
     {
